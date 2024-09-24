945d8ce59189e97e69787c36ac0ae898e5084523
Benson Pierre <145082418+bensfull@users.noreply.github.com>
2024-09-24 07:10:00 -0300

    commit init

---
+++ index.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 10);
+console.log(resultadoMultiplicacao); 
+
+const mensagemSaudacao = saudar("Maria");
+console.log(mensagemSaudacao); 
